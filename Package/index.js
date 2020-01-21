7de74a360c33dbac16d6ebf4c81230e9cf7f9114
ph0ph0 <phillipalanlee@googlemail.com>
2020-01-21 15:42:43 +0000

    m

--- Package/index.js
+++ Package/index.js
@@ -10,61 +10,63 @@ exports.handler = async event => {
   const SubjectTable = "Subject-or2wpavvmbatbhfn7vwoynk4c4-dev";
 
   //Get event details
-  const {
-    sourceBucket,
-    sourceKey,
-    destinationBucket,
-    largeAvatarDestinationKey,
-    smallAvatarDestinationKey,
-    userId
-  } = eventDetails(event);
+  //Get Subject
+  //Update Subject table
+  // const {
+  //   sourceBucket,
+  //   sourceKey,
+  //   destinationBucket,
+  //   largeAvatarDestinationKey,
+  //   smallAvatarDestinationKey,
+  //   userId
+  // } = eventDetails(event);
 
-  try {
-    //Download the newly added avatar from storage
-    console.log("Processing avatar uploaded to storage");
+  // try {
+  //   //Download the newly added avatar from storage
+  //   console.log("Processing avatar uploaded to storage");
 
-    const avatar = await downloadImage(sourceKey, sourceBucket);
+  //   const avatar = await downloadImage(sourceKey, sourceBucket);
 
-    //Compress the avatar image to a smaller size
-    const avatars = await avatarsGenerator(avatar);
-    const largeAvatar = avatars.largeAvatar;
-    const smallAvatar = avatars.smallAvatar;
+  //   //Compress the avatar image to a smaller size
+  //   const avatars = await avatarsGenerator(avatar);
+  //   const largeAvatar = avatars.largeAvatar;
+  //   const smallAvatar = avatars.smallAvatar;
 
-    //Upload the avatars
-    await uploadImage(
-      largeAvatar,
-      largeAvatarDestinationKey,
-      destinationBucket
-    );
-    await uploadImage(
-      smallAvatar,
-      smallAvatarDestinationKey,
-      destinationBucket
-    );
+  //   //Upload the avatars
+  //   await uploadImage(
+  //     largeAvatar,
+  //     largeAvatarDestinationKey,
+  //     destinationBucket
+  //   );
+  //   await uploadImage(
+  //     smallAvatar,
+  //     smallAvatarDestinationKey,
+  //     destinationBucket
+  //   );
 
-    console.log(
-      "Uploaded the compressed avatars to Storage! Creating Picture object.."
-    );
+  //   console.log(
+  //     "Uploaded the compressed avatars to Storage! Creating Picture object.."
+  //   );
 
-    //Create picture object in dDB
-    const pictureObject = await pictureObjectGenerator(
-      largeAvatarDestinationKey,
-      destinationBucket,
-      userId
-    );
+  //   //Create picture object in dDB
+  //   const pictureObject = await pictureObjectGenerator(
+  //     largeAvatarDestinationKey,
+  //     destinationBucket,
+  //     userId
+  //   );
 
-    //The avatar picture object will have the same id as the user
-    const pictureObjectId = userId;
+  //   //The avatar picture object will have the same id as the user
+  //   const pictureObjectId = userId;
 
-    console.log("Created Picture object! Updating User object...");
-    console.log("pictureObjectID " + pictureObjectId + " , userId " + userId);
+  //   console.log("Created Picture object! Updating User object...");
+  //   console.log("pictureObjectID " + pictureObjectId + " , userId " + userId);
 
-    //Update the user object with the userAvatarId so that the avatar is bound to the user
-    await updateUserObject(userId, pictureObjectId);
+  //   //Update the user object with the userAvatarId so that the avatar is bound to the user
+  //   await updateUserObject(userId, pictureObjectId);
 
-    console.log("DONE ALL!");
-  } catch (error) {
-    console.log("ERROR!: " + error);
-    throw error;
-  }
+  //   console.log("DONE ALL!");
+  // } catch (error) {
+  //   console.log("ERROR!: " + error);
+  //   throw error;
+  // }
 };
