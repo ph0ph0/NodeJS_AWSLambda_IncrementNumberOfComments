16cf7c9ffb0076789b95cdbd3811418958b777f5
ph0ph0 <phillipalanlee@googlemail.com>
2020-01-22 11:53:36 +0000

    finalised the function

--- Package/Utils/index.js
+++ Package/Utils/index.js
@@ -1,15 +1,14 @@
-module.exports.eventDetails = event => {
-  //   console.log("Getting event details...");
-  //   const records = event.Records;
-  //   console.log("Records: %j", records);
-  //   const zeroth = records[0];
-  //   console.log("Zeroth: %j", zeroth);
-  //   const dDB = records[0].dynamodb;
-  //   console.log("dDB: %j", dDB);
-  //   const newImage = dDB.NewImage;
-  //   console.log("newImage: %j", newImage);
-  //   const subjectId = newImage.subjectId.S;
-  //   console.log("subjectId!!!: %j", subjectId);
-  const subjectId = event.Records[0].dynamodb.NewImage.subjectId.S;
-  return subjectId;
+module.exports.eventDetails = record => {
+  // const dDB = record.dynamodb;
+  // console.log("dDB: %j", dDB);
+  // const newImage = dDB.NewImage;
+  // console.log("newImage: %j", newImage);
+  // const subjectId = newImage.subjectId.S;
+  // console.log("subjectId!!!: %j", subjectId);
+  try {
+    const subjectId = record.dynamodb.NewImage.subjectId.S;
+    return subjectId;
+  } catch (error) {
+    throw error;
+  }
 };
--- Package/index.js
+++ Package/index.js
@@ -4,13 +4,29 @@
 const { eventDetails } = require("./Utils");
 const { subjectUpdater } = require("./SubjectObjectUpdater");
 
-exports.handler = async event => {
+exports.handler = (event, context, callback) => {
   console.log("Intercepted event: %j", event);
 
-  const subjectId = await eventDetails(event);
-  console.log("Got subject id: " + subjectId);
+  try {
+    event.Records.forEach(record => {
+      if (record.eventName == "INSERT") {
+        console.log("Processing record: %j", record);
+        const subjectId = eventDetails(record);
+        console.log("Got subject id: " + subjectId);
 
-  const updateSubjectObject = await subjectUpdater(subjectId);
-  console.log("Updated the object!!!: %j", updateSubjectObject);
-  return;
+        const updateSubjectObject = subjectUpdater(subjectId);
+        console.log("Updated the object!");
+      } else {
+        console.log("Event was not an insert event, aborting");
+      }
+    });
+
+    console.log("Successfully processed " + event.Records.length + "records");
+    callback(null, event);
+  } catch (error) {
+    console.log("Error running lambda: " + error);
+    //By passing null as the first argument, we tell the lambda not to retry.
+    //Otherwise, it will retry endlessly for 24 hours...
+    callback(null, error);
+  }
 };
